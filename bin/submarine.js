27dc7f4d42a871a5db108e69a56661581b249739
Mu-An Chiou <me@muanchiou.com>
2014-10-19 22:15:31 +0900

    Prevent wrong dir

--- bin/submarine.js
+++ bin/submarine.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
+var fs = require('fs')
 var submarine = require('../index.js')
-submarine(process.argv[2], process.argv[3])
+var input_dir = process.argv[2]
+var output_dir = process.argv[2]
+
+if(!fs.existsSync('./' + input_dir)) {
+  console.warn('\033[91mThe input directory you specify does not exist.\033[0m')
+} else {
+  submarine(process.argv[2], process.argv[3])
+}
