4f0cc399429a92d157398b932f5f475c103a1232
Mu-An Chiou <me@muanchiou.com>
2014-10-20 16:39:04 +0900

    ./ to process.cwd()

--- index.js
+++ index.js
@@ -9,29 +9,30 @@ function submarine(input_dir, output_dir, header, footer) {
   template = getTemplate(header, footer)
 
   // Create output_dir if doesn't exist
-  if(!fs.existsSync(path.resolve('./', output_dir))) {
-    fs.mkdir(path.resolve('./', output_dir), function(err) {
+  if(!fs.existsSync(path.resolve(process.cwd(), output_dir))) {
+    fs.mkdir(path.resolve(process.cwd(), output_dir), function(err) {
       if (err) return console.log(err)
     })
   }
 
   // Create html files
-  fs.readdir(path.resolve('./', input_dir), function(err, files) {
+  fs.readdir(path.resolve(process.cwd(), input_dir), function(err, files) {
     if (err) return console.log(err)
     files = files.filter(function(n) { return n.match(/.+\..+$/) }).sort()
 
     // Write index
-    fs.writeFile(path.resolve('./', output_dir, 'index.html'), indexHTML(files), function (err) {
+    fs.writeFile(path.resolve(process.cwd(), output_dir, 'index.html'), indexHTML(files), function (err) {
       if (err) return console.log(err)
     })
 
     // Write markdowns into HTML
     files.forEach(function(name) {
-      fs.readFile(path.resolve('./', input_dir, name), function(err, file) {
+      fs.readFile(path.resolve(process.cwd(), input_dir, name), function(err, file) {
+        if (err) return console.log(err)
         var index = files.indexOf(name)
         var html = generateHTML(file.toString(), files[index-1], files[index+1])
         var filename = getFilename(name)
-        fs.writeFile(path.resolve('./', output_dir, filename + '.html'), html, function (err) {
+        fs.writeFile(path.resolve(process.cwd(), output_dir, filename + '.html'), html, function (err) {
           if (err) return console.log(err)
         })
       })
